a7741965e6b9d3cb62078a18218e0efcdbb8cece
saharshg29 <saharshg29@gmail.com>
2022-04-07 17:08:20 +0530

    Signin page

--- client/src/Components/Signin.jsx/index.jsx
+++ client/src/Components/Signin.jsx/index.jsx
@@ -1,12 +1,28 @@
-import "./index.scss";
+import "./index.css";
 
 const SignIn = () => {
   return (
     <>
       <div className="SignIn">
-          <div className="username">Username: <input type="text" name="Username" id="" /></div>
-          <div className="password">Password: <input type="password" name="Password" id="" /></div>
+        <div className="fields">
+          <div className="logo"></div>
+          <div className="username">
+            Username:{" "}
+            <input placeholder="Username" type="text" name="Username" id="" />
+          </div>
+          <div className="password">
+            Password:{" "}
+            <input
+              placeholder="Password"
+              type="password"
+              name="Password"
+              id=""
+            />
+          </div>
           <div className="button">Log In!</div>
+
+          <span>Forget Password?</span>
+        </div>
       </div>
     </>
   );
