58b93b84f3cd99181177ab0a109d592b1261ce6b
saharshg29 <saharshg29@gmail.com>
2022-04-07 16:01:41 +0530

    starting from beginning

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -1,12 +1,3 @@
-import { Route, Routes } from "react-router-dom";
-import SignUp from "./Components/Screen/SignUp/SignUp";
-import SignIn from "./Components/Screen/SignIn/SignIn";
-import Admin from "./Components/Admin/Admin";
-import AddStudent from "./Components/Admin/AddStudent";
-import AddTeacher from "./Components/Admin/AddTeacher";
-import AddSubject from "./Components/Admin/AddSubject";
-import React from "react";
-import Edit from "./Components/Student/Edit";
 
 export default function AllRoutes() {
   return (
--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,9 @@
 import "./App.css";
-import Navbar from "./Components/Screen/Nav/Navbar";
-import Footer from "./Components/Screen/Footer/Footer";
 import AllRoutes from "./AllRoutes";
-import Left from "./Components/Left/Left";
-import Right from "./Components/Right/Right";
 function App() {
   return (
     <>
-      <div className="sticky-top mb-2">
-        <Navbar />
-      </div>
-      <div className="row">
-        <div className="col-md m-1 bg-danger ">
-          <Left></Left>
-        </div>
-        <div className="col-md-8">
-          <AllRoutes />
-        </div>
-        <div className="col-md m-1 bg-info">
-          <Right></Right>
-        </div>
-      </div>
-      <div className="sticky-bottom mt-2">
-        <Footer />
-      </div>
+      <AllRoutes />
     </>
   );
 }
